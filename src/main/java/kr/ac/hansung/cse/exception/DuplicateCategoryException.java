b305054d2a60598c6dd55a98fb0b8ad4b8850b1b
kimdoyun <kimsagagg@gmail.com>
2026-04-14 01:11:43 +0900

    feat:CategoryService 비즈니스 로직 추가 및 예외 상황 처리

--- src/main/java/kr/ac/hansung/cse/exception/DuplicateCategoryException.java
+++ src/main/java/kr/ac/hansung/cse/exception/DuplicateCategoryException.java
@@ -1,6 +1,6 @@
 package kr.ac.hansung.cse.exception;
 
-class DuplicateCategoryException extends RuntimeException {
+public class DuplicateCategoryException extends RuntimeException {
     public DuplicateCategoryException(String name) {
         super("이미 존재하는 카테고리입니다: " + name);
     }
